929d07aaf7c79b11ca647499d775eb8cce63fc36
Alex Nuccio <anuccio1@gmail.com>
2016-02-25 15:36:03 -0800

    added fetcher code

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-var nuget = require('nuget.js');
-
-// https://www.nuget.org/api/v2/Packages?%24filter=Id%20eq%20%27elmah%27
+var Nug = require('./nuget.js');
+var nuget = new Nug();
 
 nuget.resolve('nuget+elmah').then(function(locator) {
 	// expect latest version (1.2.2) nuget+elmah$1.2.2
--- nuget.js
+++ nuget.js
@@ -2,7 +2,8 @@ var Promise = require('bluebird'),
     BaseFetcher = require('./BaseFetcher.js'),
     inherits = require('inherits'),
     _ = require('underscore'),
-    request = require('request-promise');
+    request = require('request-promise'),
+    parseString = require('xml2js').parseString;
 
 
 var NugetFetcher = function(config) {
@@ -31,20 +32,77 @@ NugetFetcher.prototype.resolvePackage = function(locator, force_resolve) {
     if(!locator.package.value || (!force_resolve && locator.package.value && locator.package.resolved)) {
       return locator; // if package is null or already resolved, skip resolution
     }
-    // TODO: parse urls, registry formats, auth, etc...
+    //URL IN THIS FORMAT if Version is explicit: https://www.nuget.org/api/v2/Packages(Id='<packageName>',Version='<version>')
+    //For no version: https://www.nuget.org/api/v2/Packages?%24filter=Id%20eq%20%27elmah%27
 
-    return request('https://www.nuget.org/api/v2/Packages?%24filter=Id%20eq%20%27' + locator.package.value + '%27').then(function (error, response, body) {
-      if (!error && response.statusCode === 200) {
-        return locator;
+    var nugetUrl;
+    var locatorRevision = locator.revision.full;
+    //if revision is listed
+    if (locatorRevision) {
+      if (locatorRevision.charAt(0) === '[' || locatorRevision.charAt(0) === '(') {
+        //TODO: resolve this part
+        nugetUrl = 'https://www.nuget.org/api/v2/Packages(Id=\'' + locator.package.value + '\',Version=\'' + locator.revision.full + '\')';
       } else {
-        if(!locator.package.value || !locator.package.resolved) throw new Error("No package found with spec: " + locator);
-        return locator;
+        nugetUrl = 'https://www.nuget.org/api/v2/Packages(Id=\'' + locator.package.value + '\',Version=\'' + locator.revision.full + '\')';
       }
+    } else {
+      var nugetUrl = 'https://www.nuget.org/api/v2/Packages?';
+      nugetUrl += '%24filter=Id%20eq%20%27' + locator.package.value + '%27';
+    }
+    return self.fetchPackage(nugetUrl, locator).then(function(data) {
+      locator.package = {
+        value: data.title,
+        full: data.title,
+        resolved: true,
+        metadata: data
+      };
+      return locator;
     });
-
+   }).then(function(locator) {
+    // catch unresolved packages
+     if(!locator.package.value || !locator.package.resolved) throw new Error("No package found with spec: " + locator);
+     return locator;
   });
 };
 
+NugetFetcher.prototype.fetchPackage = function(url, locator) {
+  return new Promise(function (resolve, reject) {
+    try {
+      request({uri: url, resolveWithFullResponse: false})
+      .then(function (response) {
+        parseString(response, function (err, result) {
+          if (err) {
+            return reject(new Error('Package with that name does not exist'));
+          }
+          var retJSON = {};
+          var allEntries = result.feed.entry;
+
+          for (var i=0; i<allEntries.length; ++i) {
+            var allProperties = allEntries[i]['m:properties'][0];
+            var title = allProperties['d:Title'][0];
+            if (title.toUpperCase() === locator.package.value.toUpperCase()) {  //only if the title matches
+              retJSON = allProperties;
+              retJSON.title = locator.package.value;
+              break;
+            }
+          }
+          if (retJSON.title) {
+            return resolve(retJSON);
+          } else {
+            return reject(new Error('Package with that name does not exist'));
+          }
+        });
+      })
+      .catch(function(err) {
+        return reject(new Error('Error Requesting URL. Package with that name does not exist'));
+      });
+    } catch(e) {
+      return reject(new Error("Invalid Nuget package url: " + url));
+    }
+  });
+};
+
+module.exports = NugetFetcher;
 
 // NugetFetcher.prototype.resolveRevision = function(locator, force_resolve) {
 //   return NugetFetcher.super_.prototype.resolveRevision.apply(this, arguments).then(function(locator) {
